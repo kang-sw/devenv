898ca36b8e7da24108b29096abe6ccdb105b61ab
kang-sw <ki6080@gmail.com>
2026-02-26 01:07:05 +0900

    feat/nvim: change explorer as floating window

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,6 +7,20 @@ return {
     picker = {
       sources = {
         explorer = {
+          jump = { close = true },
+          layout = {
+            layout = {
+              backdrop = false,
+              width = 0.4,
+              min_width = 40,
+              height = 0.8,
+              position = "float", -- 핵심!
+              border = "rounded",
+              box = "vertical",
+              { win = "input", height = 1, border = "rounded", title = "{title}", title_pos = "center" },
+              { win = "list", border = "none" },
+            },
+          },
           win = {
             list = {
               keys = {
