cf26481809f8601e04ea6486c76e2ab6200e5aee
kang-sw <ki6080@gmail.com>
2026-03-03 22:12:06 +0900

    feat(nvim): set claudecode default width as 110

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,13 @@
+return {
+  "coder/claudecode.nvim",
+  opts = {
+    terminal = {
+      snacks_win_opts = {
+        width = 110,
+      },
+    },
+    diff_opts = {
+      open_in_new_tab = true,
+    },
+  },
+}
